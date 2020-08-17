eba5146e662e3e206221cea81ca017d2f0eadc2f
hasnaa messaoudi <hasnaa.messaoudi@gmail.com>
2020-08-17 22:57:56 +0000

    use Template Literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,7 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  if (actual === expected) console.log("\u{2705}\u{2705}\u{2705}Assertion Passed: " + actual + " === " + expected);
-  else console.log("\u{274C}\u{274C}\u{274C}Assertion Failed: " + actual + " !== " + expected);
+  if (actual === expected) console.log("\u{2705}\u{2705}\u{2705}Assertion Passed: ${actual} === ${expected}");
+  else console.log("\u{274C}\u{274C}\u{274C}Assertion Failed: ${actual} !== ${expected}");
 };
 
 
