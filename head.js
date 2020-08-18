491f6166f1a8b4b22f7ecb0b3e9b77761a4d52f2
hasnaa messaoudi <hasnaa.messaoudi@gmail.com>
2020-08-18 01:38:48 +0000

    commit eslint fixes

--- head.js
+++ head.js
@@ -2,9 +2,9 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) console.log(`\u{2705}\u{2705}\u{2705}Assertion Passed: ${actual} === ${expected}`);
   else console.log(`\u{274C}\u{274C}\u{274C}Assertion Failed: ${actual} !== ${expected}`);
-}; 
+};
 
-const head = function(array){
+const head = function(array) {
   return array[0];
 };
 
--- tail.js
+++ tail.js
@@ -2,26 +2,22 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) console.log(`\u{2705}\u{2705}\u{2705}Assertion Passed: ${actual} === ${expected}`);
   else console.log(`\u{274C}\u{274C}\u{274C}Assertion Failed: ${actual} !== ${expected}`);
-}; 
+};
 
 const assertArrayEqual = function(array1, array2) {
   let equal = true;
-  if (array1.length !== array2.length) {
-    console.log(`\u{274C}\u{274C}\u{274C}Assertion Failed: ${array1} !== ${array2}`);
-  }
+  if (array1.length !== array2.length) console.log(`\u{274C}\u{274C}\u{274C}Assertion Failed: ${array1} !== ${array2}`);
   else {
-    for (let i = 0; i < array1.length; i++){
+    for (let i = 0; i < array1.length; i++)
       if (array1[i] !== array2[i]) {
         equal = false; break;
       }
-    } 
     if (equal) console.log(`\u{2705}\u{2705}\u{2705}Assertion Passed: ${array1} === ${array2}`);
     else console.log(`\u{274C}\u{274C}\u{274C}Assertion Failed: ${array1} !== ${array2}`);
   }
+};
 
-}; 
-
-const tail = function(array){
+const tail = function(array) {
   let result = [];
   for (let i = 1; i < array.length; i++) {
     result.push(array[i]);
