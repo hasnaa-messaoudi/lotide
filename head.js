131fd117778b4861416641351bcd55d5b6e0b08c
hasnaa messaoudi <hasnaa.messaoudi@gmail.com>
2020-08-18 00:11:06 +0000

    Lotide head and assertion

--- head.js
+++ head.js
@@ -2,11 +2,16 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) console.log(`\u{2705}\u{2705}\u{2705}Assertion Passed: ${actual} === ${expected}`);
   else console.log(`\u{274C}\u{274C}\u{274C}Assertion Failed: ${actual} !== ${expected}`);
+}; 
+
+const head = function(array){
+  return array[0];
 };
 
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(1, 1);
-assertEqual(1, 2);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head([5,6,7]), 7);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["HelloLabs", "Lighthouse", "Labs"]), "Hello");
+
