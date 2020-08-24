adcfda555af7ef088edde7ebfa96fbe970b0d202
hasnaa messaoudi <hasnaa.messaoudi@gmail.com>
2020-08-24 05:58:31 +0000

    separate test code for middle

--- middle.js
+++ middle.js
@@ -1,18 +1,3 @@
-const eqArrays = function(array1, array2) {
-  let result = (array1.length === array2.length) ? true : false;
-  if (result) {
-    for (let i = 0; i < array1.length; i++)
-      if (array1[i] !== array2[i]) {
-        result = false; break;
-      }
-  }
-  return result;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) console.log(`\u{2705}\u{2705}\u{2705}Assertion Passed: ${array1} === ${array2}`);
-  else console.log(`\u{274C}\u{274C}\u{274C}Assertion Failed: ${array1} !== ${array2}`);
-};
 
 const middle = function(array) {
   let result = [];
@@ -24,11 +9,4 @@ const middle = function(array) {
   return result;
 };
 
-// TEST CODE
-
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
