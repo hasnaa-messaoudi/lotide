00826dd9dbc4f55d814bb12160a4baf911845294
hasnaa messaoudi <hasnaa.messaoudi@gmail.com>
2020-08-24 05:38:06 +0000

    separate test code for eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) console.log(`\u{2705}\u{2705}\u{2705}Assertion Passed: ${actual} === ${expected}`);
-  else console.log(`\u{274C}\u{274C}\u{274C}Assertion Failed: ${actual} !== ${expected}`);
-};
-
-
-
 const eqArrays = function(array1, array2) {
   let result = (array1.length === array2.length) ? true : false;
   if (result) {
@@ -16,9 +9,4 @@ const eqArrays = function(array1, array2) {
   return result;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
